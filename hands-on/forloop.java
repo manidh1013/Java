6e067600cb5fa2e2111b6e8304956a9731f74094
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-11 12:13:04 +0530

    Add files via upload

---
+++ hands-on/forloop.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int i,b;
+		for(i=1;i<=10;i++) {
+		b= a*i;
+		System.out.println(a +"*"+ i +"="+b);
+		
+			
+	}
+
+	}}
