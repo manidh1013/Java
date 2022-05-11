f061a944bd68aafa637f3318338bae9d6d900e19
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-11 10:44:44 +0530

    Add files via upload

---
+++ hands-on/arithmeticoperation.java
@@ -0,0 +1,44 @@
+package corejava;
+
+public class arithmeticoperation {
+	
+	public static void main(String[] args ) {
+		
+		//addition
+		int a ,b,c ;
+		a=100 ;
+		b=2;
+		c=a+b;
+		
+		System.out.println("sum="+c);
+		
+		//subtraction
+		int d;
+		d=a-b;
+		System.out.println("sub="+d);
+		
+		//multiplication
+		int e;
+		e=a*b;
+		System.out.println("multiplication="+e);
+		
+		//division
+		int f;
+		f=a/2;
+		System.out.println("division="+f);
+		
+		//percentage
+	
+	int g,h,i;
+		g=66;
+		h=100;
+		i=(g*100)/h;
+		
+		
+		System.out.println("percentage="+i);
+		
+		
+		
+	}
+
+}
