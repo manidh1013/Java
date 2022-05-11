824e7c312645d36dfacf30f52e5b3d90deb45a3b
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-11 12:38:09 +0530

    Add files via upload

---
+++ hands-on/ifesle.java
@@ -0,0 +1,27 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class ifesle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your age : ");
+		int age = sc.nextInt();
+		  if(age >=18) {
+			  System.out.println("you are eligible for voting");
+		  
+		   }
+		  else{
+			  
+			  System.out.println("you are not eligible for voting;");
+		  }
+		  
+		 
+		
+		
+	}
+
+}
