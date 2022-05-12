cded7f38284fea81a65a70598f123d63a70dfd6b
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-12 12:08:30 +0530

    Add files via upload

---
+++ practical/wideningcastingimplicit.java
@@ -0,0 +1,22 @@
+package corejava;
+
+public class wideningcastingimplicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i= 200;
+		
+		//automatic type conversion
+		long l =i;
+		
+		//automatic type conversion 
+		float f=l;
+		System.out.println("int value :"+i);
+		System.out.println("long value :"+l);
+		System.out.println("float value :"+f);
+		
+
+	}
+
+}
