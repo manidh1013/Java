915e6ecd01202670c345e802787c3afb93468c17
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-12 12:48:44 +0530

    Add files via upload

---
+++ hands-on/implicitexplicit.java
@@ -0,0 +1,27 @@
+package corejava;
+
+public class implicitexplicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//implicit coversion
+		int a = 44;
+		
+		double d= a;
+	 System.out.println("double value:"+d);
+	   
+	 
+	 
+	 //explicit coversion
+	 double e =21.03;
+	
+	 int i = (int)e;
+	 System.out.println("int value:"+i);
+	 
+		
+		
+		
+	}
+
+}
