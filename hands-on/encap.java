0a6eed0ff6bbc84a82249324f75e573510c7772a
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-16 13:42:03 +0530

    Add files via upload

---
+++ hands-on/encap.java
@@ -0,0 +1,35 @@
+package corejava;
+
+public class encap {
+
+	private int id ;
+	private String name ;
+	private String adress;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAdress() {
+		return adress;
+	}
+	
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+		
+	
+}
---
+++ hands-on/encap1.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class encap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		encap e = new encap();
+		
+		e.setId(122);
+		e.setName("manish");
+		e.setAdress("govandi");
+		
+		System.out.println("id:"+ e.getId());
+		System.out.println("name :"+ e.getName());
+		System.out.println("aadress: "+ e.getAdress());
+		
+	}
+
+}
