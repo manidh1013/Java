1bbbe083b961b09c97a1c9038e56bd70190b4e67
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-16 13:40:49 +0530

    Add files via upload

---
+++ practical/abstraction.java
@@ -0,0 +1,10 @@
+package corejava;
+
+//creatin abstract class
+	abstract class abstraction {
+		
+		//creating variables
+		 public abstract int Multiply(int n1, int n2); 
+		 public abstract int Multiplyone(int n1, int n2, int n3); 
+	}
+	
