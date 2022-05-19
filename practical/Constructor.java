4b73fac8929b5140a1ec24523ac1ce384852f11f
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-19 11:53:39 +0530

    Add files via upload

---
+++ practical/Constructor.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Constructor {
+
+	Constructor()
+	{
+		System.out.println("this is default constructor/ no argument constructor");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Constructor c = new Constructor();
+		
+	
+	}
+
+}
