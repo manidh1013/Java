a933a03a2a57d2a6ddaf1ec7caabe2beab299a5e
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-19 11:00:36 +0530

    Add files via upload

---
+++ practical/ContinueState.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class ContinueState {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<10; i++) {
+			if(i==4) {
+				continue;
+				
+			}
+			else if (i==7) {
+				break;
+				
+			}
+			System.out.println(i);
+	}
+
+	}}
