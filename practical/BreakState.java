de03d7583039a4cb2edcde601d72103082a008c4
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-19 10:49:32 +0530

    Add files via upload

---
+++ practical/BreakState.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class BreakState {
+
+	public static void main(String[] args) {
+	
+		for (int i=0; i<10; i++) {
+			if(i==4) {
+				break;
+				
+			}
+			System.out.println(i);
+			
+		}
+			
+		
+	}
+
+}
