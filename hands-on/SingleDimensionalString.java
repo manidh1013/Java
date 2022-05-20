c9687dbe1df831874724594a5fb7dda16ac25bdd
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-20 13:00:36 +0530

    Add files via upload

---
+++ hands-on/SingleDimensionalString.java
@@ -0,0 +1,28 @@
+package corejava;
+
+public class SingleDimensionalString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	String a[] = new String[10];
+		a[0]="wet"; //initialization
+		a[1]="vfr";
+		a[2]="gter";
+		a[3]="bol";
+		a[4]="vfr";
+		a[5]="gtedr";
+		a[6]="sf";
+		a[7]="red";
+		a[8]="ver";
+		a[9]="fg";
+		
+		//traversing array
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+	}
+
+}
+}
