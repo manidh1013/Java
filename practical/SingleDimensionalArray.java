74b877d030bad2b9febb9296aa630101eb62f167
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-05-20 10:55:11 +0530

    Add files via upload

---
+++ practical/SingleDimensionalArray.java
@@ -0,0 +1,28 @@
+package corejava;
+
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+		
+		// declaration and instantiation
+		int a[] = new int[10];
+		a[0]=10; //initialization
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		a[5]=60;
+		a[6]=70;
+		a[7]=80;
+		a[8]=90;
+		a[9]=100;
+		
+		//traversing array
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+				
+		}
+		
+	}
+
+}
