1d2cb0a97c12eefed9b1893e5a8a71d348f18882
MANISH PATIL <98407880+manidh1013@users.noreply.github.com>
2022-06-24 10:28:59 +0530

    Add files via upload

---
+++ LabSession/23rd June/SnakeRun.java
@@ -0,0 +1,12 @@
+package LabSession;
+
+public class SnakeRun {
+
+		public static void main(String args[])throws Exception{
+			
+			// creating a object of SnakeCase
+			SnakeCase sn=new SnakeCase();
+			//printing output
+			System.out.println(sn.camelToSnake("DoSelect"));
+				}
+			}
